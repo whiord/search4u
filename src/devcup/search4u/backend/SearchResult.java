cf5241ea9d7992a331a9e65c53b8d2ad97d5ff75
Artem Rodichev <rodichev.artem@gmail.com>
2013-12-05 00:51:08 +0400

    backend: searcher improvement(wildcard, relevance, stability, etc)

--- src/devcup/search4u/backend/SearchResult.java
+++ src/devcup/search4u/backend/SearchResult.java
@@ -10,46 +10,58 @@ import java.util.List;
 
 
 public class SearchResult {
-    private String query;
-    private Map<String, List<ObjectPair<Integer, String>>> fragments; // key - document path, value - pair(offset, fragment text)
-    private List<ObjectPair<String, String>> documents; // key - document path, value - document highlighting text
-    
-    public void setQuery(String query) {
-        this.query = query;
+	private String query;
+	private Map<String, List<ObjectPair<Integer, String>>> fragments; // key - document path, value - pair(offset, fragment text)
+	private List<ObjectPair<String, String>> documents; // key - document path, value - document highlighting text
+	private List<String> relevDocsPath;
+	
+   public void addRelevDocsPath(String path) {
+       if (relevDocsPath == null) {
+           relevDocsPath = new ArrayList<String>();
+       }
+       relevDocsPath.add(path);
     }
     
-    public String getQuery() {
-        return query;
+    public List<String> getRelevDocsPath() {
+        return relevDocsPath;
     }
-    
-    public void addDocuments(List<ObjectPair<String, String>> docs) {
-        documents = docs;
-    }
+	
+	public void setQuery(String query) {
+		this.query = query;
+	}
+	
+	public String getQuery() {
+		return query;
+	}
+	
+	public void addDocuments(List<ObjectPair<String, String>> docs) {
+	    documents = docs;
+	}
    
-    public List<ObjectPair<String, String>> getDocuments() {
-        return documents;
-    }
-    
-    public void addDocumentsFragment(String path, Integer offset, String fragmentText) {
-        if (fragments == null) {
-            fragments = new HashMap<String, List<ObjectPair<Integer, String>>>();
-        } 
-        
-        if (fragments.containsKey(path)) {
-            ObjectPair<Integer, String> pair = new ObjectPair<Integer, String>();
-            pair.setObjects(offset, fragmentText);
-            fragments.get(path).add(pair);
-        } else {
-            List<ObjectPair<Integer, String>> value = new ArrayList<ObjectPair<Integer, String>>();
-            ObjectPair<Integer, String> pair = new ObjectPair<Integer, String>();
-            pair.setObjects(offset, fragmentText);
-            value.add(pair);
-            fragments.put(path, value);
-        }
-    }
-    
-    public Map<String, List<ObjectPair<Integer, String>>> getDocumentsFragments() {
-        return fragments;
-    }
+	public List<ObjectPair<String, String>> getDocuments() {
+		return documents;
+	}
+	
+	public void addDocumentsFragment(String path, Integer offset, String fragmentText) {
+		if (fragments == null) {
+			fragments = new HashMap<String, List<ObjectPair<Integer, String>>>();
+		} 
+		
+		if (fragments.containsKey(path)) {
+			ObjectPair<Integer, String> pair = new ObjectPair<Integer, String>();
+			pair.setObjects(offset, fragmentText);
+			fragments.get(path).add(pair);
+		} else {
+			List<ObjectPair<Integer, String>> value = new ArrayList<ObjectPair<Integer, String>>();
+			ObjectPair<Integer, String> pair = new ObjectPair<Integer, String>();
+			pair.setObjects(offset, fragmentText);
+			value.add(pair);
+			fragments.put(path, value);
+		}
+	}
+	
+	public Map<String, List<ObjectPair<Integer, String>>> getDocumentsFragments() {
+		return fragments;
+	}
 
 }
--- src/devcup/search4u/backend/Searcher.java
+++ src/devcup/search4u/backend/Searcher.java
@@ -3,6 +3,8 @@ package devcup.search4u.backend;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -29,6 +31,8 @@ import org.apache.lucene.search.highlight.TokenSources;
 import org.apache.lucene.store.FSDirectory;
 import org.apache.lucene.util.Version;
 
+import org.apache.lucene.search.highlight.WeightedSpanTermExtractor;
+
 import com.ontos.core.miner.util.ObjectPair;
 
 public class Searcher {
@@ -36,72 +40,169 @@ public class Searcher {
     private RussianAnalyzer analyzer;
     private IndexSearcher searcher;
     private IndexReader reader;
-    private Map<String, String> documents;
+    private Map<String, String> documents;   
+    private SearchCallback callback;
 
-    public Searcher(String indexDirName, SearchCallback handler) throws IOException {
+    public Searcher(String indexDirName, SearchCallback callback) throws IOException {
         FSDirectory dir = FSDirectory.open(new File(indexDirName));
         reader = IndexReader.open(dir);
         searcher = new IndexSearcher(reader);
         analyzer = new RussianAnalyzer();
         documents = new HashMap<String, String>();
+        this.callback = callback;
     }
     
     private String processQuery(String queryStr) {
-        queryStr = queryStr.replaceAll(" ", " AND ");
-        return queryStr;    
+    	queryStr = queryStr.replaceAll(" ", " AND ");
+    	return queryStr;	
     }
+   
     
     private List<ObjectPair<String, String>> formingSortedRelevDocs(Map<String, String> docs) {
-        List<ObjectPair<String, String>> docsList = new ArrayList<ObjectPair<String, String>>();
-        for (Map.Entry<String, String> entry : docs.entrySet()) {
-            ObjectPair<String, String> pair = new ObjectPair<String, String>();
-            pair.setObjects(entry.getKey(), entry.getValue());
-            docsList.add(pair);
-        }
-        return docsList;
+    	List<ObjectPair<String, String>> docsList = new ArrayList<ObjectPair<String, String>>();
+    	for (Map.Entry<String, String> entry : docs.entrySet()) {
+    	    ObjectPair<String, String> pair = new ObjectPair<String, String>();
+			pair.setObjects(entry.getKey(), entry.getValue());
+			docsList.add(pair);
+    	}
+    	Collections.sort(docsList, new RelevanceComparator());
+    	return docsList;
     }
     
     public List<SearchResult> search(List<String> queries, boolean isWildcardSearch) throws ParseException, IOException, InvalidTokenOffsetsException
-    {          
+    {	  	
+        if (callback != null) callback.setTotalProgress(queries.size() + 1);
         int max_hits_number = 1000000;
         QueryParser qParser = new QueryParser(Version.LUCENE_36, "text", analyzer);
         qParser.setAllowLeadingWildcard(isWildcardSearch);
+        qParser.setDefaultOperator(QueryParser.Operator.AND);
         for (String queryStr : queries) {
-            queryStr = processQuery(queryStr);
-            Query query = qParser.parse(queryStr);
-            TopDocs topDocs = searcher.search(query, max_hits_number);
-            System.out.println("Documents found: " + topDocs.scoreDocs.length);
-    
-            if (topDocs.scoreDocs.length == 0) {
-                System.out.println("No documents found for query: " + queryStr);
-                continue;
+            String initQuery = queryStr;
+            if (!isWildcardSearch) {
+                queryStr = queryStr.replaceAll("[^a-zA-Z0-9а-яА-ЯёЁ\\s]", "");
+                //queryStr = processQuery(queryStr);
             }
-            
-            SimpleHTMLFormatter htmlFormatter = new SimpleHTMLFormatter();
-            Highlighter highlighter = new Highlighter(htmlFormatter, new QueryScorer(query));
-            highlighter.setTextFragmenter(new NullFragmenter());
+	        Query query = qParser.parse(queryStr);
+	        TopDocs topDocs = searcher.search(query, max_hits_number);
+	        System.out.println("Documents found: " + topDocs.scoreDocs.length);
+	
+	        if (topDocs.scoreDocs.length == 0) {
+	            if (callback != null) callback.writeLog(LogLevel.INFO, "No documents found for query: " + initQuery);
+	        	System.out.println("No documents found for query: " + initQuery);
+	            continue;
+	        }
+	        
+	        SimpleHTMLFormatter htmlFormatter = new SimpleHTMLFormatter();
+	        Highlighter highlighter = new Highlighter(htmlFormatter, new QueryScorer(query));
+	        highlighter.setTextFragmenter(new NullFragmenter());
 
-            for (ScoreDoc scoreDoc : topDocs.scoreDocs) {
-                Document d = searcher.doc(scoreDoc.doc);
-                String path = d.get("path");
-                
-                String text = (documents.containsKey(path) ? documents.get(path) : d.get("text"));
-                String hightlightText = highlighter.getBestFragment(analyzer, "", text);
-                documents.put(path, hightlightText);
-            }
+	        for (ScoreDoc scoreDoc : topDocs.scoreDocs) {
+	            Document d = searcher.doc(scoreDoc.doc);
+	            String path = d.get("path");
+	            
+	            String text = (documents.containsKey(path) ? documents.get(path) : d.get("text"));
+	            String hightlightText = text;
+            	try {
+            	    hightlightText = highlighter.getBestFragment(analyzer, "", text);
+            	} catch (NoClassDefFoundError e) {
+            	    if (callback != null) callback.writeLog(LogLevel.ERROR, e.getMessage());
+            	}
+            	documents.put(path, hightlightText);
+	        }
+	        if (callback != null) callback.setCurrentProgress(1);
         }
         
         List<SearchResult> results = getAllFragments(queries, isWildcardSearch);
         
         if (results.size() > 0) {
-            List<ObjectPair<String, String>> docs = formingSortedRelevDocs(documents);
-            results.get(0).addDocuments(docs);
+        	List<ObjectPair<String, String>> docs = formingSortedRelevDocs(documents);
+        	results.get(0).addDocuments(docs);
         }
         return results;
     }
-    
+	
     private int getWordCount(String str) {
-        Pattern p = Pattern.compile(" ");
+    	Pattern p = Pattern.compile(" ");
+        Matcher m = p.matcher(str);
+        int count = 1;
+        while (m.find()){
+        	count +=1;
+        }
+    	return count;
+    }
+    
+    private List<SearchResult> getAllFragments(List<String> queries, boolean isWildcardSearch) throws ParseException, IOException, InvalidTokenOffsetsException
+    {
+    	List<SearchResult> results = new ArrayList<SearchResult>(); 	
+    	
+    	int max_hits_number = 1000000;
+        QueryParser qParser = new QueryParser(Version.LUCENE_36, "text", analyzer);
+        qParser.setAllowLeadingWildcard(isWildcardSearch);
+        qParser.setDefaultOperator(QueryParser.Operator.AND);
+        for (String queryStr : queries) {
+        	SearchResult result = new SearchResult();
+        	result.setQuery(queryStr);
+        	
+        	if (!isWildcardSearch) {
+        	    queryStr = queryStr.replaceAll("[^a-zA-Z0-9а-яА-ЯёЁ\\s]", "");
+        	    //queryStr = processQuery(queryStr);
+        	}
+	        Query query = qParser.parse(queryStr);
+	        TopDocs topDocs = searcher.search(query, max_hits_number);
+	
+	        if (topDocs.scoreDocs.length == 0) {
+	            continue;
+	        }
+	        
+	        SimpleHTMLFormatter htmlFormatter = new SimpleHTMLFormatter();
+	        Highlighter highlighter = new Highlighter(htmlFormatter, new QueryScorer(query));
+
+	        for (ScoreDoc scoreDoc : topDocs.scoreDocs) {
+	            Document d = searcher.doc(scoreDoc.doc);
+	            String path = d.get("path");
+	            String text = d.get("text");
+	            
+	            TokenStream tokenStream = TokenSources.getAnyTokenStream(searcher.getIndexReader(), scoreDoc.doc, "text", analyzer);
+	            TextFragment[] frag = highlighter.getBestTextFragments(tokenStream, text, false, max_hits_number);
+	            for (int i = 0; i < frag.length; i++) {
+	            	int wordCount = getWordCount(result.getQuery());
+	                if ((frag[i] != null) && (frag[i].getScore() >= wordCount)) {
+	                	String fragmentText = frag[i].toString();
+	                	int offset = 0;
+	                	result.addRelevDocsPath(path);
+	                    result.addDocumentsFragment(path, offset, fragmentText);
+	                }
+	            }
+	        }
+	        results.add(result);
+        }
+        if (callback != null) callback.setCurrentProgress(1);
+        return results;
+    }
+    
+	
+	public static void main(String[] args) throws IOException, ParseException, InvalidTokenOffsetsException {		
+		String indexDirName = "D:\\test_index";
+		Searcher docSearcher = new Searcher(indexDirName, null);
+		
+		List<String> qStrs = new ArrayList<String>();
+		//qStrs.add("ООО \\\"Рог и копытом\\\"");
+		//qStrs.add("\"встреча директором\"~");
+		qStrs.add("Рога и копыта");
+		//qStrs.add("Группа компаний ПИК"); 
+		qStrs.add("Петром Сергеевичем");
+		boolean isWildcardSearch = false;
+		List<SearchResult> results = docSearcher.search(qStrs, false);
+				
+		System.out.println("Finish search");
+	}
+
+}
+
+class RelevanceComparator implements Comparator<ObjectPair<String, String>> {
+
+    private int getRelev(String str) {
+        Pattern p = Pattern.compile("<B>");
         Matcher m = p.matcher(str);
         int count = 1;
         while (m.find()){
@@ -110,62 +211,9 @@ public class Searcher {
         return count;
     }
     
-    private List<SearchResult> getAllFragments(List<String> queries, boolean isWildcardSearch) throws ParseException, IOException, InvalidTokenOffsetsException
-    {
-        List<SearchResult> results = new ArrayList<SearchResult>();     
-        
-        int max_hits_number = 1000000;
-        QueryParser qParser = new QueryParser(Version.LUCENE_36, "text", analyzer);
-        qParser.setAllowLeadingWildcard(isWildcardSearch);
-        for (String queryStr : queries) {
-            SearchResult result = new SearchResult();
-            result.setQuery(queryStr);
-            
-            queryStr = processQuery(queryStr);
-            Query query = qParser.parse(queryStr);
-            TopDocs topDocs = searcher.search(query, max_hits_number);
-    
-            if (topDocs.scoreDocs.length == 0) {
-                continue;
-            }
-            
-            SimpleHTMLFormatter htmlFormatter = new SimpleHTMLFormatter();
-            Highlighter highlighter = new Highlighter(htmlFormatter, new QueryScorer(query));
-
-            for (ScoreDoc scoreDoc : topDocs.scoreDocs) {
-                Document d = searcher.doc(scoreDoc.doc);
-                String path = d.get("path");
-                String text = d.get("text");
-                
-                TokenStream tokenStream = TokenSources.getAnyTokenStream(searcher.getIndexReader(), scoreDoc.doc, "text", analyzer);
-                TextFragment[] frag = highlighter.getBestTextFragments(tokenStream, text, false, max_hits_number);
-                for (int i = 0; i < frag.length; i++) {
-                    int wordCount = getWordCount(result.getQuery());
-                    if ((frag[i] != null) && (frag[i].getScore() >= wordCount)) {
-                        String fragmentText = frag[i].toString();
-                        int offset = 0;
-                        result.addDocumentsFragment(path, offset, fragmentText);
-                    }
-                }    
-            }
-            results.add(result);
-        }
-
-        return results;
+    public int compare(ObjectPair<String, String> obj1, ObjectPair<String, String> obj2) {
+        int relev1 = getRelev(obj1.getSecond());
+        int relev2 = getRelev(obj2.getSecond());
+        return relev1 > relev2 ? -1 : relev1 == relev2 ? 0 : 1;
     }
-    
-    
-    public static void main(String[] args) throws IOException, ParseException, InvalidTokenOffsetsException {        
-        String indexDirName = "D:\\test_index";
-        Searcher docSearcher = new Searcher(indexDirName, null);
-        
-        List<String> qStrs = new ArrayList<String>();
-        qStrs.add("Рогов и копыт");
-        qStrs.add("Группа компаний Пик"); 
-        boolean isWildcardSearch = false;
-        List<SearchResult> results = docSearcher.search(qStrs, false);
-                
-        System.out.println("Finish search");
-    }
-
 }
